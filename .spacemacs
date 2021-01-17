3a0e9156b956ca07a165615874787cedf798cabd
kangxiaoning <kxnmei@163.com>
2021-01-17 16:40:57 +0800

    fix .spacemacs.template url

--- .spacemacs
+++ .spacemacs
@@ -31,6 +31,7 @@ This function should only modify configuration layer settings."
    dotspacemacs-configuration-layer-path '()
 
    ;; List of configuration layers to load.
+   
    ;; ----------------------------------------------------------------
    ;; Example of useful layers you may want to use right away.
    ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
--- update-dot-spacemacs.sh
+++ update-dot-spacemacs.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-wget https://github.com/syl20bnr/spacemacs/blob/develop/core/templates/.spacemacs.template
-mv .spacemacs .spacemacs.bak
-python3 update-dot-spacemacs.py > .spacemacs
+TEMPLATE="https://raw.githubusercontent.com/syl20bnr/spacemacs/develop/core/templates/.spacemacs.template"
+wget -q $TEMPLATE -O .spacemacs.template
+
+if [ $? -ne 0 ]; then
+    echo "download .spacemacs.template failed"
+    exit 1
+else
+    echo "download .spacemacs.template successful"
+    mv -v .spacemacs .spacemacs.bak
+    python3 update-dot-spacemacs.py > .spacemacs && echo "generate .spacemacs successful" || echo "generate .spacemacs failed"
+fi
